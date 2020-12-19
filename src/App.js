ce396d549cf44811125f0690a57a96e23e236b9a
Marzooque <mmarzooque@gmail.com>
2020-12-19 13:23:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './main.css';
+import diagun from './assets/diagun.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+function App () {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+    return (
+      <div className="container">
+        <div className="main-container">
+        <div className="imagecontainer">
+          <img src={diagun} alt={diagun} />
+          <div className="bcont">
+          <button onClick={()=> setIsOpen(!isOpen)} className="b1">ADD TO BAG</button>
+          <button onClick={()=> setIsOpen(!isOpen)}   className="b2">WISH LIST</button>
+          {isOpen ? (
+          <div className="Modal">
+            <p className="modhead">Select size</p>
+            <div className="sizcont">
+            <button className="s1">XS</button>
+            <button className="s2">S</button>
+            <button className="s3">M</button>
+            <button className="s4">L</button>
+            <button className="s5">XL</button>
+            </div>
+            </div>
+          ):null}
+          </div>
+          
+
+          </div>
+          <p className="t1">Vero Moda</p>
+          <p className="t2">Women regular fit</p>
+
+          <div className="line">
+          <p className="t3">SR 82.39</p>
+          <p className="t4">SR 82.39</p>
+          <p className="t5">(116.6 OFF)</p>
+          </div>
+       
+          </div>
+           </div>
+    )
+  }
+
+
+export default App
